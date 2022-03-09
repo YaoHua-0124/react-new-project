859ce732fdafa1bf3b02fa62c26ccacbc8565d48
YaoHua-0124 <79439989+YaoHua-0124@users.noreply.github.com>
2022-03-09 15:50:42 +0800

    Update index.jsx

--- my-app/src/component/Footer/index.jsx
+++ my-app/src/component/Footer/index.jsx
@@ -2,11 +2,31 @@ import React, { Component } from 'react'
 import './Footer.css'
 
 export default class Footer extends Component {
+
+  // 选中所有的事项
+  selectdeAll = (event) => {
+    this.props.selectdeAll(event.target.checked)
+  }
+
+  // 删除选中的事项
+  deleteSelectd = (completedItem) => {
+    return () => {
+      this.props.deleteSelectd(completedItem)
+    }
+  }
+
   render() {
+    const { todos } = this.props
+    let completedItem = []
+    for (let index = 0; index < todos.length; index++) {
+      if (todos[index].isDone === true) completedItem.push(todos[index])
+    }
+    let isAllSelected = todos.length === completedItem.length ? true : false
     return (
       <div className='footer'>
-        <input className='checkbox' name="selectAll" type="checkbox"/>
-        <span className='showInfo'>已完成0 / 全部4</span>
+        <input type="checkbox" checked={isAllSelected} onChange={this.selectdeAll} className='checkbox' name="selectAll" />
+        <span className='showInfo'>已完成{completedItem.length} / 全部{todos.length}</span>
+        <button style={{ display: completedItem.length ? 'block' : 'none' }} onClick={this.deleteSelectd(completedItem)}>清除已完成任务</button>
       </div>
     )
   }
